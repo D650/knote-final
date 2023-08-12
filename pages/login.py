54910d61f0cb0123195ed1dda9effe2bd2e50ebe
D650 <daham.s.karunadasa@gmail.com>
2023-08-12 10:55:13 -0500

    Update login.py

--- pages/login.py
+++ pages/login.py
@@ -20,13 +20,12 @@ if "credentials" not in st.session_state:
 
 if "code" in st.experimental_get_query_params():
     auth_code = st.experimental_get_query_params()["code"][0]
-    st.write(auth_code)
     flow.fetch_token(authorization_response=auth_code)
     st.session_state.credentials = flow.credentials
 
 
 if "credentials" in st.session_state:
-    credentials = st.session_state.credentials
+    credentials = flow.credentials
     service = googleapiclient.discovery.build('plus', 'v1', credentials=credentials)
     user_info = service.people().get(userId='me').execute()
 
