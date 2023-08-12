3491eb922ce1d1004fd7de477e3a386d41e1c216
D650 <daham.s.karunadasa@gmail.com>
2023-08-12 11:10:01 -0500

    Update login.py

--- pages/login.py
+++ pages/login.py
@@ -2,7 +2,7 @@ import streamlit as st
 from google_auth_oauthlib.flow import Flow
 import googleapiclient.discovery
 import json
-
+import secrets
 
 flow = Flow.from_client_config(
     json.loads(st.secrets["logintextkey"]),
@@ -10,26 +10,29 @@ flow = Flow.from_client_config(
     redirect_uri= st.secrets["ouath_redirect_url"],
 )
 
+state = secrets.token_urlsafe(16)
 
 if "credentials" not in st.session_state:
-    authorization_url, _ = flow.authorization_url(prompt='consent', state=st.session_state['_oauth2_state'])
-    # authorization_url, _ = flow.authorization_url(prompt='consent', state=flow.state)
-    # authorization_url, _ = flow.authorization_url(prompt='consent')
 
+    authorization_url, _ = flow.authorization_url(prompt='consent')
     st.write(f"Click [here]({authorization_url}) to authenticate with Google.")
 
 
 if "code" in st.experimental_get_query_params():
-    auth_code = st.experimental_get_query_params()["code"][0]
-    st.session_state['_oauth2_state'] = st.experimental_get_query_params()["state"][0]
-    # auth_code = st.experimental_get_query_params()["code"][0]
-    flow.fetch_token(authorization_response=auth_code, state=st.experimental_get_query_params()["state"][0])
-    # flow.fetch_token(authorization_response=auth_code)
-    st.session_state.credentials = flow.credentials
+        auth_code = st.experimental_get_query_params()["code"][0]
+        # Retrieve the state parameter from the query parameters
+        state = st.experimental_get_query_params()["state"][0]
+
+        # # Check if the state parameter matches the expected value
+        # if state != expected_state:
+        #     raise ValueError("Mismatching state parameter")
+
+        flow.fetch_token(authorization_response=auth_code)
+        st.session_state.credentials = flow.credentials
 
 
 if "credentials" in st.session_state:
-    credentials = flow.credentials
+    credentials = st.session_state.credentials
     service = googleapiclient.discovery.build('plus', 'v1', credentials=credentials)
     user_info = service.people().get(userId='me').execute()
 
