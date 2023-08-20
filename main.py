caed29a90b6a151869751d1532e6deb13443c50e
D650 <daham.s.karunadasa@gmail.com>
2023-08-20 12:43:44 -0500

    Update main.py

--- main.py
+++ main.py
@@ -169,7 +169,7 @@ else:
             try:
                 storage_context = StorageContext.from_defaults(persist_dir=f'{AWS_BUCKET_NAME}/{user}', fs=s3)
             except FileNotFoundError:
-                st.error("You have not uploaded any files. Please upload some and come back to this page.")
+                st.error("You have not uploaded any files. Please upload some, press refresh and come back to this page.")
                 st.stop()
             index = load_index_from_storage(storage_context)
 
